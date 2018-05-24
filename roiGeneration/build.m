bde413bc357e178ff028d48752b105dbc8821249
Bradley Caron <bacaron@h2.carbonate.uits.iu.edu>
2018-05-23 22:54:33 -0400

    recompiled

--- roiGeneration/build.m
+++
@@ -1,6 +0,0 @@
-addpath(genpath('/N/u/brlife/git/vistasoft'))
-addpath(genpath('/N/u/brlife/git/encode'))
-addpath(genpath('/N/u/brlife/git/jsonlab'))
-addpath(genpath('/N/u/brlife/git/spm'))
-mcc -m -R -nodisplay -d ROI roiGeneration
-exit
