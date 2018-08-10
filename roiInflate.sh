0823097062eb97060fb35c705b01bbec37d0afd3
Franco Pestilli <frakkopesto@gmail.com>
2018-08-10 11:02:20 -0400

    Edited Help at top of functions

--- roiInflate.sh
+++ roiInflate.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
-## This script uses AFNI's (Taylor PA, Saad ZS (2013).  FATCAT: (An Efficient) Functional And Tractographic Connectivity Analysis Toolbox. Brain 
-## Connectivity 3(5):523-535. https://afni.nimh.nih.gov/) 3dROIMaker function to a) remove the white matter mask from the cortical segmentation 
-## inputted (i.e. freesurfer or parcellation; BE CAREFUL: REMOVES SUBCORTICAL ROIS) and b) inflates the ROIs by n voxels into the white matter 
-## based on user input (option for no inflation is also built in). The output of this will then passed into a matlab function (roiGeneration.m) to 
-## create nifti's for each ROI requested by the user, which can then be fed into a ROI to ROI tracking app (brainlife.io; www.github.com/brain-life/
+##
+## Inflate ROIs limiting artifacts that can occlude the white-matter tissue within the gyri.
+##
+## This script uses AFNI's (Taylor PA, Saad ZS (2013).  
+## - FATCAT: (An Efficient) Functional And Tractographic Connectivity Analysis Toolbox. Brain Connectivity 3(5):523-535. https://afni.nimh.nih.gov/) 
+## - 3dROIMaker function to 
+##    a) remove the white matter mask from the cortical segmentation inputted (i.e. freesurfer or parcellation, BE CAREFUL: REMOVES SUBCORTICAL ROIS) and 
+##    b) inflate the ROIs by n voxels into the white matter based on user input (option for no inflation is also built in). 
+##
+## The output of this will then passed into a matlab function (roiGeneration.m) to create nifti's for each ROI requested by the user, which can then be fed into a ROI to ROI tracking app (brainlife.io; www.github.com/brain-life/
 ## app-roi2roitracking).
 
 INFLATE=`jq -r '.inflate' config.json`
