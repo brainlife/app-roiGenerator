a72abceb50a914150797a9818f8e86d8b30df3d9
Brad Caron <bacaron@iu.edu>
2019-02-01 18:30:47 -0500

    Create brainmask.sh

---
+++ brainmask.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Setting file paths"
+# Grab the config.json inputs
+dwi=`jq -r '.dwi' config.json`;
+bvals=`jq -r '.bvals' config.json`;
+bvecs=`jq -r '.bvecs' config.json`;
+echo "Files loaded"
+
+# Create b0
+select_dwi_vols \
+	${dwi} \
+	${bvals} \
+	nodif.nii.gz \
+	0;
+
+# Brain extraction before alignment
+bet nodif.nii.gz \
+	nodif_brain \
+	-f 0.4 \
+	-g 0 \
+	-m;
+  
+mv nodif_brain_mask.nii.gz mask.nii.gz;
+
+echo "brainmask creation complete"
