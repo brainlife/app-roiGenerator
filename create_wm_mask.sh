66caf61ffba94f2637208b69184f4030a1740ae3
Brad Caron <bacaron@iu.edu>
2019-10-20 17:41:11 -0400

    Update create_wm_mask.sh

--- create_wm_mask.sh
+++ create_wm_mask.sh
@@ -41,7 +41,7 @@ fi
 if [[ ${prf} == "false" ]]; then
 	echo "no visual field mapping"
 else
-        mri_label2vol --seg ${prfDir}/varea.nii.gz  --temp $input_nii_gz --regheader ${prf}/varea.nii.gz --o varea_dwi.nii.gz
+        mri_label2vol --seg ${prfDir}/varea.nii.gz  --temp $input_nii_gz --regheader ${prfDir}/varea.nii.gz --o varea_dwi.nii.gz
 fi
 
 # convert hippocampal nuclei mgz to nifti: to do later!
