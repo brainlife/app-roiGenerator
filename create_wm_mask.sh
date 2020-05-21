74dca5fff01a9c5762e4a05e59c06d93c75681e1
Brad Caron <bacaron@iu.edu>
2020-05-21 11:32:13 -0400

    REason: update for newer versions of thalamic nuclei segmentation

--- create_wm_mask.sh
+++ create_wm_mask.sh
@@ -26,7 +26,7 @@ mri_binarize --i ${inputparc}+aseg.nii.gz --min 1 --o mask_anat.nii.gz
 mri_binarize --i ${inputparc}+aseg.nii.gz --o wm_anat.nii.gz --match 2 41 16 17 28 60 51 53 12 52 13 18 54 50 11 251 252 253 254 255 10 49 46 7
 
 # convert thalamic nuclei mgz to nifti
-mri_label2vol --seg $fsurfer/mri/ThalamicNuclei.v10.T1.FSvoxelSpace.mgz --temp $input_nii_gz --regheader $fsurfer/mri/ThalamicNuclei.v10.T1.FSvoxelSpace.mgz --o thalamicNuclei.nii.gz
+mri_label2vol --seg $fsurfer/mri/ThalamicNuclei.*.T1.FSvoxelSpace.mgz --temp $input_nii_gz --regheader $fsurfer/mri/ThalamicNuclei.*.T1.FSvoxelSpace.mgz --o thalamicNuclei.nii.gz
 
 # convert visual area segmentation
 mri_label2vol --seg ${prfDir} --temp $input_nii_gz --regheader ${prfDir} --o varea_dwi.nii.gz
