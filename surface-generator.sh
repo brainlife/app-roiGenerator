578de21bc6c67b0805ce1ae0eb00199b171f0647
Brad Caron <bacaron@iu.edu>
2020-09-04 20:36:40 -0400

    Update surface-generator.sh

--- surface-generator.sh
+++ surface-generator.sh
@@ -5,9 +5,8 @@ set -x
 set -e
 
 # parse inputs
+varea=`jq -r '.prfDir' config.json`
 prfSurfacesDir=`jq -r '.prfSurfacesDir' config.json`
-lh_annot=`jq -r '.lh_annot' config.json`
-rh_annot=`jq -r '.rh_annot' config.json`
 minDegree=`jq -r '.min_degree' config.json` # min degree for binning of eccentricity
 maxDegree=`jq -r '.max_degree' config.json` # max degree for binning of eccentricity
 freesurfer=`jq -r '.freesurfer' config.json`
@@ -15,8 +14,8 @@ input_nii_gz=`jq -r '.input_nifti' config.json`
 hemispheres="lh rh"
 
 # glasser ROIS
-visROIs="2 3 4 5 6 7 8 11 12 14 17 18 19 20 21 22 23 24 50 120 122 127 128 134 136 137 138 139 141 142 143 144 147 153 154 155 156 157 158 159 160 161 164"
-visROINames="v1 mst v6 v2 v3 v4 v8 fef pef v3a v7 ips1 ffc v3b lo1 lo2 pit mt mip pres pros pha1 pha3 te1p tf te2p pht ph tpoj2 tpoj3 dvt pgp ip0 v6a vmv1 vmv3 pha2 v4t fst v3cd lo3 vmv2 vvc "
+visROIs="1 2 3 4 5 6 7 8 9 10 11 12"
+visROINames="v1 v2 v3 hv4 vO1 vO2 lO1 lO2 tO1 tO2 v3b v3a"
 visROIs=($visROIs)
 visROINames=($visROINames)
 
@@ -46,14 +45,15 @@ do
 		[ ! -f ./${hemi}.Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.func.gii ] && mri_binarize --i ./${hemi}.eccentricity.func.gii --min ${minDegree[$DEG]} --max ${maxDegree[$DEG]} --o ./${hemi}.Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.func.gii
 	done
 	
-	# create glasser rois
-	parc=$(eval "echo \$${hemi}_annot")
+	# convert varea surface to gifti 
+	[ ! -f ${hemi}.varea.func.gii ] && mris_convert -c ${prfSurfacesDir}/${hemi}.varea ${freesurfer}/surf/${hemi}.pial ${hemi}.varea.func.gii
+
+	# create visual area rois
 	for i in ${!visROIs[@]}
 	do
 		echo "${visROINames[$i]}"
-		[ ! -f ${hemi}.${visROINames[$i]}.func.gii ] && wb_command -gifti-label-to-roi ${parc} ${hemi}.${visROINames[$i]}.func.gii -key ${visROIs[$i]}
-		[ ! -f ./rois/rois/${hemi}.${visROINames[$i]}.nii.gz ] && mri_surf2vol --o ./rois/rois/${hemi}.${visROINames[$i]}.nii.gz --subject ./ --so ${freesurfer}/surf/${hemi}.pial ./${hemi}.${visROINames[$i]}.func.gii && mri_vol2vol --mov ./rois/rois/${hemi}.${visROINames[$i]}.nii.gz --targ ${input_nii_gz} --regheader --o ./rois/rois/${hemi}.${visROINames[$i]}.nii.gz --nearest
-
+		[ ! -f ${hemi}.${visROINames[$i]}.func.gii ] && mri_binarize --i ./${hemi}.varea.func.gii --match ${visROIs[$i]} --o ./${hemi}.${visROINames[$i]}.func.gii
+		
 		# extract eccentricity bins for each glasser visual roi
 		for DEG in ${!minDegree[@]}; do
 			# genereate eccentricity bin surfaces and multiply eccentricities by roi
