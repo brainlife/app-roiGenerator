6db94a80c27a33385101468bd1eabddb71e9011f
Brad Caron <bacaron@iu.edu>
2020-10-14 13:33:22 -0400

    Update create_wm_mask.sh

--- create_wm_mask.sh
+++ create_wm_mask.sh
@@ -13,6 +13,7 @@ mri_label2vol --seg $fsurfer/mri/${inputparc}+aseg.mgz --temp $input_nii_gz --re
 mri_binarize --i ${inputparc}+aseg.nii.gz --min 1 --o mask_anat.nii.gz
 mri_binarize --i ${inputparc}+aseg.nii.gz --o wm_anat.nii.gz --match 2 41 16 17 28 60 51 53 12 52 13 18 54 50 11 251 252 253 254 255 10 49 46 7
 input_nii_gz="${inputparc}+aseg.nii.gz"
+mri_convert $fsurfer/mri/brainmask.mgz ./mask.nii.gz
 
 # convert thalamic nuclei mgz to nifti
 mri_label2vol --seg $fsurfer/mri/ThalamicNuclei.*.T1.FSvoxelSpace.mgz --temp $input_nii_gz --regheader $fsurfer/mri/ThalamicNuclei.*.T1.FSvoxelSpace.mgz --o thalamicNuclei.nii.gz
