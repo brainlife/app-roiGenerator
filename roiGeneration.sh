7d72b1bef8cd5f86581b25b0bac278de88a3c05c
Brad Caron <bacaron@iu.edu>
2020-12-03 20:55:01 -0500

    reason: small fix

--- roiGeneration.sh
+++ roiGeneration.sh
@@ -145,6 +145,23 @@ fi
 3dTstat -argmax -prefix allroiss.nii.gz all_pre.nii.gz
 3dcalc -byte -a allroiss.nii.gz -expr 'a' -prefix allrois_byte.nii.gz
 
+# rewrite roi names
+mv ROI008109.nii.gz ROIlh.lgn.nii.gz
+mv ROI008209.nii.gz ROIrh.lgn.nii.gz
+mv ROI0001.nii.gz ROIv1.nii.gz
+mv ROI0002.nii.gz ROIv2.nii.gz
+mv ROI0003.nii.gz ROIv3.nii.gz
+mv ROI0004.nii.gz ROIhV4.nii.gz
+mv ROI0005.nii.gz ROIvO1.nii.gz
+mv ROI0006.nii.gz ROIvO2.nii.gz
+mv ROI0007.nii.gz ROIlO1.nii.gz
+mv ROI0008.nii.gz ROIlO2.nii.gz
+mv ROI0009.nii.gz ROItO1.nii.gz
+mv ROI00010.nii.gz ROItO2.nii.gz
+mv ROI00011.nii.gz ROIv3b.nii.gz
+mv ROI00012.nii.gz ROIv3a.nii.gz
+mv ROI085.nii.gz ROIoptic-chiasm.nii.gz
+
 if [[ -z ${mergeROIsL} ]] || [[ -z ${mergeROIsR} ]]; then
         echo "no merging of rois"
 else
@@ -161,7 +178,6 @@ else
         	3dcalc -byte -a ${mergename}L_nonbyte.nii.gz -expr 'a' -prefix ${mergename}L_allbytes.nii.gz
         	3dcalc -a ${mergename}L_allbytes.nii.gz -expr 'step(a)' -prefix ROIlh.${mergename}.nii.gz
                 mv *`ls ${mergeArrayL}`* ./rois/rois/
-
 	fi
 	if [[ ! -z ${mergeROIsR} ]]; then
 		mergeArrayR=""
@@ -177,26 +193,8 @@ else
 	fi
 fi
 
-# rewrite roi names
-mv ROI008109.nii.gz ROIlh.lgn.nii.gz
-mv ROI008209.nii.gz ROIrh.lgn.nii.gz
-mv ROI0001.nii.gz ROIv1.nii.gz
-mv ROI0002.nii.gz ROIv2.nii.gz
-mv ROI0003.nii.gz ROIv3.nii.gz
-mv ROI0004.nii.gz ROIhV4.nii.gz
-mv ROI0005.nii.gz ROIvO1.nii.gz
-mv ROI0006.nii.gz ROIvO2.nii.gz
-mv ROI0007.nii.gz ROIlO1.nii.gz
-mv ROI0008.nii.gz ROIlO2.nii.gz
-mv ROI0009.nii.gz ROItO1.nii.gz
-mv ROI0010.nii.gz ROItO2.nii.gz
-mv ROI0011.nii.gz ROIv3b.nii.gz
-mv ROI0012.nii.gz ROIv3a.nii.gz
-mv ROI085.nii.gz ROIoptic-chiasm.nii.gz
-
 # move exclusion files as to not include in parcellation (significant overlap)
 mv *${mergename}*.nii.gz ./rois/rois/
-
 
 # create key.txt for parcellation
 FILES=(`echo "*ROI*.nii.gz"`)
