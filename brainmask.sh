77f367a330978d545685476d09522a41cf4996b9
Brad Caron <bacaron@iu.edu>
2022-05-06 15:39:23 -0400

    Update brainmask.sh

--- brainmask.sh
+++ brainmask.sh
@@ -3,18 +3,12 @@
 echo "Setting file paths"
 
 # parse whether input is dtiinit or dwi
-dwi=`jq -r '.dwi' config.json`;
-bvals=`jq -r '.bvals' config.json`;
-bvecs=`jq -r '.bvecs' config.json`;
+fmri=`jq -r '.bold' config.json
 
 echo "Files loaded"
 
-# Create b0
-select_dwi_vols \
-	${dwi} \
-	${bvals} \
-	nodif.nii.gz \
-	0;
+# select first volume
+fslselectvols -i ${fmri} -o nodif.nii.gz --vols=0
 
 # Brain extraction before alignment
 bet nodif.nii.gz \
