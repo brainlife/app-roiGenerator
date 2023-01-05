4abc49c1d399573d18cb6bef970fb1d709194c45
Brad Caron <bacaron245@gmail.com>
2023-01-05 16:27:25 -0600

    Update roiMerge.sh
    changes to zeroDataset generation so merged ROIs should be consistently same size

--- roiMerge.sh
+++ roiMerge.sh
@@ -24,7 +24,7 @@ mergenames=($mergename)
 rois_avail=(`ls ${rois}`)
 
 # create parcellation of all rois
-3dcalc -a ${rois}/${rois_avail[0]} -prefix zeroDataset.nii.gz -expr '0'
+
 
 if [[ -z ${mergeROIsL} ]] || [[ -z ${mergeROIsR} ]]; then
         echo "no merging of rois"
@@ -38,33 +38,44 @@ else
 			while read -r line; do
 				mergeArray+=("$line")
 			done <<< "$mergeROIsL"
+						
 			for (( i=0; i<${#mergeArray[@]}; i++ ))
 			do
 				mergeArrayL=""
 				mergeName=${mergenames[$i]}
 
 				for j in ${mergeArray[$i]}
-				do 
+				do
+					if [[ ${j} -eq 0 ]]; then
+						first_name=`echo ${rois}/ROI$j.nii.gz`
+					fi
 					mergeArrayL=$mergeArrayL" `echo ${rois}/ROI$j.nii.gz`"
 				done
+							
+				[ ! -f zeroDataset.nii.gz ] && 3dcalc -a ${rois}/${first_name} -prefix zeroDataset.nii.gz -expr '0'
+
 				echo ${mergeArrayL} ${mergeName}
 				3dTcat -prefix merge_preL.nii.gz zeroDataset.nii.gz `ls ${mergeArrayL}`
 		        	3dTstat -argmax -prefix ${mergeName}L_nonbyte.nii.gz merge_preL.nii.gz
 		        	3dcalc -byte -a ${mergeName}L_nonbyte.nii.gz -expr 'a' -prefix ${mergeName}L_allbytes.nii.gz
 		        	3dcalc -a ${mergeName}L_allbytes.nii.gz -expr 'step(a)' -prefix ROI${mergeName}_L.nii.gz
-		        	rm -rf merge_preL.nii.gz ${mergeName}L_nonbyte.nii.gz ${mergeName}L_allbytes.nii.gz
+		        	rm -rf merge_preL.nii.gz ${mergeName}L_nonbyte.nii.gz ${mergeName}L_allbytes.nii.gz zeroDataset.nii.gz
 	        	done
         	else
 			mergeArrayL=""
 			for i in "${mergeL[@]}"
 			do
+				if [[ ${i} -eq 0 ]]; then
+					first_name=`echo ${rois}/ROI"$i".nii.gz`
+				fi
 				mergeArrayL="$mergeArrayL `echo ${rois}/ROI"$i".nii.gz`"
 			done
-			
+			[ ! -f zeroDataset.nii.gz ] && 3dcalc -a ${rois}/${first_name} -prefix zeroDataset.nii.gz -expr '0'
 			3dTcat -prefix merge_preL.nii.gz zeroDataset.nii.gz `ls ${mergeArrayL}`
 	        	3dTstat -argmax -prefix ${mergename}L_nonbyte.nii.gz merge_preL.nii.gz
 	        	3dcalc -byte -a ${mergename}L_nonbyte.nii.gz -expr 'a' -prefix ${mergename}L_allbytes.nii.gz
 	        	3dcalc -a ${mergename}L_allbytes.nii.gz -expr 'step(a)' -prefix ROI${mergename}_L.nii.gz
+			rm -rf zeroDataset.nii.gz
 	        fi
 	fi
 	if [[ ! -z ${mergeROIsR} ]]; then
@@ -82,26 +93,37 @@ else
 
 				for j in ${mergeArray[$i]}
 				do 
+					if [[ ${j} -eq 0 ]]; then
+						first_name=`echo ${rois}/ROI$j.nii.gz`
+					fi
 					mergeArrayR=$mergeArrayR" `echo ${rois}/ROI$j.nii.gz`"
 				done
+				
+				[ ! -f zeroDataset.nii.gz ] && 3dcalc -a ${rois}/${first_name} -prefix zeroDataset.nii.gz -expr '0'
 
 				3dTcat -prefix merge_preR.nii.gz zeroDataset.nii.gz `ls ${mergeArrayR}`
 		        	3dTstat -argmax -prefix ${mergeName}R_nonbyte.nii.gz merge_preR.nii.gz
 		        	3dcalc -byte -a ${mergeName}R_nonbyte.nii.gz -expr 'a' -prefix ${mergeName}R_allbytes.nii.gz
 		        	3dcalc -a ${mergeName}R_allbytes.nii.gz -expr 'step(a)' -prefix ROI${mergeName}_R.nii.gz
-		        	rm -rf merge_preR.nii.gz ${mergeName}R_nonbyte.nii.gz ${mergeName}R_allbytes.nii.gz
+		        	rm -rf merge_preR.nii.gz ${mergeName}R_nonbyte.nii.gz ${mergeName}R_allbytes.nii.gz zeroDataset.nii.gz
 			done
         	else
 			mergeArrayR=""
 			for i in "${mergeR[@]}"
 			do
+				if [[ ${i} -eq 0 ]]; then
+					first_name=`echo ${rois}/ROI"$i".nii.gz`
+				fi
 				mergeArrayR="$mergeArrayR `echo ${rois}/ROI*"$i".nii.gz`"
 			done
-			
+
+			[ ! -f zeroDataset.nii.gz ] && 3dcalc -a ${rois}/${first_name} -prefix zeroDataset.nii.gz -expr '0'
+
 			3dTcat -prefix merge_preR.nii.gz zeroDataset.nii.gz `ls ${mergeArrayR}`
 	        	3dTstat -argmax -prefix ${mergename}R_nonbyte.nii.gz merge_preR.nii.gz
 	        	3dcalc -byte -a ${mergename}R_nonbyte.nii.gz -expr 'a' -prefix ${mergename}R_allbytes.nii.gz
 	        	3dcalc -a ${mergename}R_allbytes.nii.gz -expr 'step(a)' -prefix ROI${mergename}_R.nii.gz
+			rm -rf zeroDataset.nii.gz
 	        fi
 	fi
 fi
