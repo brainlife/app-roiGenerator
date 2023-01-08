0446ed551844eb6cdca5e4af58cb1a4ce3bfdc5e
Brad Caron <bacaron245@gmail.com>
2023-01-08 16:32:43 -0600

    Reason: initial commit for major improvements

---
+++ combine-parcellation.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# top variables
+minDegree=`jq -r '.min_degree' config.json` # min degree for binning of eccentricity
+maxDegree=`jq -r '.max_degree' config.json` # max degree for binning of eccentricity
+
+# make degrees loopable
+minDegree=($minDegree)
+maxDegree=($maxDegree)
+
+# loop through all bins and create single volume, then multiply binary file by number of degree bins so we can create one large parcellation
+for DEG in ${!minDegree[@]}; do
+	# combine hemispheres into one single volume
+	[ ! -f Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.nii.gz ] && fslmaths lh.Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.nii.gz -add rh.Ecc${minDegree[$DEG]}to${maxDegree[$DEG]} -bin Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.nii.gz
+
+	# multiply by parcellation number (i.e. DEG; +1 because 0 index)
+	[ ! -f Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}_parc$((DEG+1)).nii.gz ] && fslmaths Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.nii.gz -mul $((DEG+1)) Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}_parc$((DEG+1)).nii.gz
+	
+	# make combination easier by creating holder variable to pass into fslmaths
+	if [[ $DEG -eq 0 ]]; then
+		holder="fslmaths Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}_parc$((DEG+1)).nii.gz"
+	else
+		holder="$holder -add Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}_parc$((DEG+1)).nii.gz"
+	fi
+done
+
+# create final parcellation
+if [ ! -f parc/parc.nii.gz ]; then
+	${holder} ./parc/parc.nii.gz
+fi
+
+# create label and key files
+FILES=(`echo "Ecc*to*_parc*.nii.gz"`)
+for i in "${!FILES[@]}"
+do
+	name=`echo ${FILES[$i]} | cut -d'_' -f1`
+	oldval=$((i+1))
+
+	newval=$oldval
+	echo -e "${oldval}\t->\t${newval}\t== ${name}" >> key.txt
+
+	# make tmp.json containing data for labels.json
+	jsonstring=`jq --arg key0 'name' --arg value0 "${name}" --arg key1 "desc" --arg value1 "value of ${newval} indicates voxel belonging to eccentricity bin ${name}" --arg key2 "voxel_value" --arg value2 ${newval} '. | .[$key0]=$value0 | .[$key1]=$value1 | .[$key2]=$value2' <<<'{}'`
+	if [ ${i} -eq 0 ]; then
+		echo -e "[\n${jsonstring}," >> tmp.json
+	elif [ ${newval} -eq ${#FILES[*]} ]; then
+		echo -e "${jsonstring}\n]" >> tmp.json
+	else
+		echo -e "${jsonstring}," >> tmp.json
+	fi
+done
+
+# move label and key files to proper location
+[ ! -f parc/label.json ] & [ -f tmp.json ] && mv tmp.json ./parc/label.json
+[ ! -f parc/key.txt ] & [ -f key.txt ] && mv key.txt ./parc/key.txt
+
+# final check
+if [ ! -f parc/parc.nii.gz ]; then
+	echo "something went wrong. check deriviatives and logs"
+	exit 1
+else
+	echo "parcellation generation complete."
+	mv *.nii.gz *.txt tmp.json *.gii *.pial ./raw/
+fi
---
+++ eccentricity-nifti-generator.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# output lines to log files and fail if error
+# set -x
+# set -e
+
+# parse inputs
+prfSurfacesDir=`jq -r '.prfSurfacesDir' config.json`
+prfVerticesDir=`jq -r '.prfVerticesDir' config.json`
+minDegree=`jq -r '.min_degree' config.json` # min degree for binning of eccentricity
+maxDegree=`jq -r '.max_degree' config.json` # max degree for binning of eccentricity
+freesurfer=`jq -r '.freesurfer' config.json`
+inputparc=`jq -r '.inputparc' config.json`
+# input_nii_gz=`jq -r '.input_nii_gz' config.json`
+hemispheres="lh rh"
+dwi=`jq -r '.dwi' config.json`
+fsurfer=`jq -r '.freesurfer' config.json`
+inputparc=`jq -r '.inputparc' config.json`
+
+# make directories
+[ ! -d parc ] && mkdir parc
+[ ! -d raw ] && mkdir raw
+
+# make degrees loopable
+minDegree=($minDegree)
+maxDegree=($maxDegree)
+
+# set up some stuff to move inputaparc to diffusion space
+input_nii_gz=$dwi
+
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+# inputaparc to diffusion space
+[ ! -f ${inputparc}+aseg.nii.gz ] && mri_label2vol --seg $fsurfer/mri/${inputparc}+aseg.mgz --temp $input_nii_gz --regheader $fsurfer/mri/${inputparc}+aseg.mgz --o ${inputparc}+aseg.nii.gz
+
+# use this as internal target for volume moves
+input_nii_gz="${inputparc}+aseg.nii.gz"
+
+# set SUBJECTS_DIR
+export SUBJECTS_DIR=${freesurfer}
+
+# move freesurfer whole-brain ribbon into diffusion space
+[ ! -f ribbon.nii.gz ] && mri_convert ${freesurfer}/mri/ribbon.mgz ./ribbon.nii.gz
+
+# loop through hemispheres and create eccentricity surfaces
+for hemi in ${hemispheres}
+do
+	echo "converting files for ${hemi}"
+
+	# move freesurfer hemisphere ribbon into diffusion space
+	[ ! -f ${hemi}.ribbon.nii.gz ] && mri_convert $freesurfer/mri/${hemi}.ribbon.mgz ./${hemi}.ribbon.nii.gz
+
+	# convert pial to gifti
+	[ ! -f ${hemi}.pial ] && mris_convert ${freesurfer}/surf/${hemi}.pial ./${hemi}.pial
+
+	# convert eccentricity surface to gifti
+	[ ! -f ${hemi}.eccentricity.func.gii ] && mris_convert -c ${prfSurfacesDir}/${hemi}.eccentricity ./${hemi}.pial ${hemi}.eccentricity.func.gii
+
+	# create mask of visual occipital lobe
+	[ ! -f ${hemi}.mask.func.gii ] && wb_command -metric-math 'x / x' -var x ${hemi}.eccentricity.func.gii ${hemi}.mask.func.gii
+
+	# loop through degrees and create individual nifti files for each bin and hemisphere
+	for DEG in ${!minDegree[@]}; do
+		# genereate eccentricity bin surfaces and mask eccentricities
+		[ ! -f ./${hemi}.Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.func.gii ] && mri_binarize --i ./${hemi}.eccentricity.func.gii --min ${minDegree[$DEG]} --max ${maxDegree[$DEG]} --o ./${hemi}.Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.func.gii && wb_command -metric-mask ./${hemi}.Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.func.gii ./${hemi}.mask.func.gii ./${hemi}.Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.func.gii
+
+		# create volume-based binned file
+		[ ! -f ./${hemi}.Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.nii.gz ] && mri_surf2vol --o ./${hemi}.Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.nii.gz --subject ./ --so ./${hemi}.pial ./${hemi}.Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.func.gii && mri_vol2vol --mov ./${hemi}.Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.nii.gz --targ ${input_nii_gz} --regheader --o ./${hemi}.Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.nii.gz --nearest
+	done
+done
